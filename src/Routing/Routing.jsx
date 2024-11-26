33b6e8ee548e67daa0889f6d3585ccb5bcd0631a
rjspyk5 <rjspyk5@gmail.com>
2024-11-27 01:49:19 +0600

    add:some images and hero section

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { createBrowserRouter } from "react-router";
 import { RootLayout } from "../layout/rootLayout";
 import { Home } from "../pages/Home/Home";
+import { Flights } from "../pages/Flights/Flights";
 
 export const Routing = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ path: "", element: <Home /> }],
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/flights", element: <Flights /> },
+    ],
   },
 ]);
--- src/component/shared/Navbar.jsx
+++ src/component/shared/Navbar.jsx
@@ -45,61 +45,63 @@ export const Navbar = () => {
     </>
   );
   return (
-    <div>
-      <div className="drawer">
-        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-          {/* Navbar */}
-          <div className="navbar justify-between bg-base-300 w-full">
-            <div className="flex-none lg:hidden">
-              <label
-                htmlFor="my-drawer-3"
-                aria-label="open sidebar"
-                className="btn btn-square btn-ghost"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block h-6 w-6 stroke-current"
+    <div className="sticky top-0 z-50">
+      <div className="">
+        <div className="drawer">
+          <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content flex flex-col">
+            {/* Navbar */}
+            <div className="navbar justify-between ] bg-[#3d3d3d49] backdrop-blur-lg absolute w-full">
+              <div className="flex-none lg:hidden">
+                <label
+                  htmlFor="my-drawer-3"
+                  aria-label="open sidebar"
+                  className="btn btn-square btn-ghost"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </label>
-            </div>
-            <div className="mx-2  px-2">
-              <Link to="/">
-                <img src={logo} className="w-20" alt="SkyJabo" />
-              </Link>
-            </div>
-            <div className="hidden flex-none lg:block">
-              <ul className="menu menu-horizontal space-x-2">
-                {/* Navbar menu content here */}
-                {menu}
-              </ul>
-            </div>
-            <div className="flex space-x-3 items-center justify-center">
-              <div className="custom-btn">Login</div>
-              <div className="custom-btn">Register</div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block h-6 w-6 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </label>
+              </div>
+              <div className="mx-2  px-2">
+                <Link to="/">
+                  <img src={logo} className="w-20" alt="SkyJabo" />
+                </Link>
+              </div>
+              <div className="hidden flex-none lg:block">
+                <ul className="menu menu-horizontal space-x-2">
+                  {/* Navbar menu content here */}
+                  {menu}
+                </ul>
+              </div>
+              <div className="flex space-x-3 items-center justify-center">
+                <div className="custom-btn">Login</div>
+                <div className="custom-btn">Register</div>
+              </div>
             </div>
           </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu bg-base-200 min-h-full w-80 p-4 space-y-2">
+              {menu}
+            </ul>
+          </div>
         </div>
-        <div className="drawer-side">
-          <label
-            htmlFor="my-drawer-3"
-            aria-label="close sidebar"
-            className="drawer-overlay"
-          ></label>
-          <ul className="menu bg-base-200 min-h-full w-80 p-4 space-y-2">
-            {menu}
-          </ul>
-        </div>
-      </div>
+      </div>{" "}
     </div>
   );
 };
