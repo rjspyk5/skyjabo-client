e613a6aa1cc510eaa7307ed179bf9192ee005a44
rjspyk5 <rjspyk5@gmail.com>
2024-11-27 18:00:45 +0600

    implement date picker with custom style and complete primary step desgin of hero section

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import img from "../../assets/images/img (21).jpg";
+import React, { forwardRef } from "react";
+import img from "../../assets/images/img (18).jpg";
 import "../../Style/style.css";
 import "./Hero.css";
 import { GiAirplaneArrival, GiAirplaneDeparture } from "react-icons/gi";
 import { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
 
 import { BsCalendarDate } from "react-icons/bs";
+import DatePicker from "react-datepicker";
 
 export const Hero = () => {
-  const [value, onChange] = useState(new Date());
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
   return (
     <div className="">
       <div
@@ -18,7 +19,7 @@ export const Hero = () => {
           backgroundImage: `url("${img}")`,
         }}
       >
-        <div className="hero-overlay bg-opacity-60 bg-black"></div>
+        <div className="hero-overlay bg-opacity-70 bg-black"></div>
 
         <div className="hero-content  text-center ">
           <div className="space-y-5">
@@ -27,7 +28,7 @@ export const Hero = () => {
               Welcome to SkyJabo! Explore your journey with seamless bookings.
             </h1>
             <div className="backdrop-blur-lg mt-5">
-              <div className="p-10 relative">
+              <div className="p-12 relative">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
                   <label className="inputt flex items-center gap-2">
                     <GiAirplaneDeparture />
@@ -49,7 +50,19 @@ export const Hero = () => {
                   </label>
 
                   <label className="inputt  flex items-center gap-2">
-                    <BsCalendarDate />
+                    <DatePicker
+                      closeOnScroll={true}
+                      toggleCalendarOnIconClick
+                      showIcon
+                      placeholderText="Select Date"
+                      selected={startDate}
+                      dateFormat="MMMM d, yyyy"
+                      onChange={(date) => setStartDate(date)}
+                      icon={
+                        <BsCalendarDate className=" bottom-[-20%] left-[-3%] cursor-pointer" />
+                      }
+                      className="calender "
+                    />
                   </label>
                 </div>
                 <button className="custom-btn2 absolute left-[39%] md:left-[44%] bottom-[-20px]">
