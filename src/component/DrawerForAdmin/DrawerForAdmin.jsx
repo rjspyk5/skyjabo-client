c477b7317aa4b21b75def27e72f44dc6132fa0cd
rjspyk5 <rjspyk5@gmail.com>
2024-11-29 18:24:29 +0600

    start desgining admin dashboard

---
+++ src/component/DrawerForAdmin/DrawerForAdmin.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const DrawerForAdmin = ({ menu }) => {
+  return (
+    <div>
+      {" "}
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+        </div>
+        <div className="drawer-side fixed w-full">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu  min-h-full w-80 backdrop-blur-3xl bg-[#57575747] p-4">
+            {" "}
+            {menu}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,52 +1,67 @@
 import React from "react";
+import { DrawerForAdmin } from "../component/DrawerForAdmin/DrawerForAdmin";
+import { useAuth } from "../hooks/useAuth";
 
 export const AdminLayout = () => {
+  const { user, logout } = useAuth();
   const menu = (
-    <ul className="menu bg-base-200 min-h-full w-80 p-4">
+    <>
       <li>
         <a>Sidebar Item 1</a>
       </li>
       <li>
         <a>Sidebar Item 2</a>
       </li>
-    </ul>
+    </>
   );
   return (
-    <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
-        {/* Navbar */}
+    <>
+      <div className="flex">
+        {/* left side menu*/}
+        <div className="w-[23%] hidden lg:block bg-white fixed top-0 h-full bg-gradient-to-br ">
+          <ul className="menu w-full   p-4">{menu} </ul>
+        </div>
+        {/* Right side */}
 
-        <div className="flex-none lg:hidden">
-          <label
-            htmlFor="my-drawer-3"
-            aria-label="open sidebar"
-            className="btn btn-square btn-ghost"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-6 w-6 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </label>
+        <div className="flex-1 flex bg-[#F4F2F3] min-h-screen lg:ml-[23%] w-full">
+          {/* Navbar */}
+          <div className="flex w-full justify-between">
+            <DrawerForAdmin menu={menu} />
+
+            <div className="dropdown dropdown-end ">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <button onClick={logout}>Logout</button>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="drawer-side">
-        <label
-          htmlFor="my-drawer-3"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        ></label>
-        {menu}
-      </div>
-    </div>
+    </>
   );
 };
