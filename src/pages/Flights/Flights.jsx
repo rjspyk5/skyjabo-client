e062e54a6ed72c88feef5c566d3c18d94157bd62
rjspyk5 <rjspyk5@gmail.com>
2024-12-01 05:10:35 +0600

    fetch flight data from database into flight component

--- src/pages/Flights/Flights.jsx
+++ src/pages/Flights/Flights.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { SearchSection } from "../../component/Hero/SearchSection/SearchSection";
 import img from "../../assets/images/low/img (1).jpg";
+import { useQuery } from "@tanstack/react-query";
+import { useAxiosPublic } from "./../../hooks/useAxiosPublic";
 
 // Dummy Data for Flights
 const flightsData = [
@@ -12,9 +14,8 @@ const flightsData = [
     arrivalTime: "00:15",
     duration: "05h 15m",
     stops: "1 stop via Goa",
-    departureCity: "Delhi",
-    arrivalCity: "Mumbai",
-    price: "$500",
+    origin: "Delhi",
+    destination: "Mumbai",
   },
   {
     id: 2,
@@ -24,13 +25,22 @@ const flightsData = [
     arrivalTime: "13:45",
     duration: "05h 15m",
     stops: "Non-stop",
-    departureCity: "Kolkata",
-    arrivalCity: "Chennai",
+    origin: "Kolkata",
+    destination: "Chennai",
     price: "$450",
   },
 ];
 
 export const Flights = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data, refetch, isLoading } = useQuery({
+    queryKey: ["flights"],
+    queryFn: async () => {
+      const result = await axiosPublic.get("/flights");
+      return result.data;
+    },
+  });
+  console.log(data);
   return (
     <div className="pt-10">
       <dialog id="my_modal_3" className="modal">
@@ -127,7 +137,7 @@ export const Flights = () => {
               type="range"
               min={0}
               max="100"
-              value="40"
+              defaultValue="40"
               className="range range-xs"
             />
           </div>
@@ -137,7 +147,7 @@ export const Flights = () => {
               type="range"
               min={0}
               max="100"
-              value="40"
+              defaultValue="40"
               className="range range-xs"
             />
           </div>
@@ -161,106 +171,97 @@ export const Flights = () => {
           <div className="flex justify-between">
             <button className="btn lg:hidden">Filter</button>
             <h6 className="text-2xl font-bold p-4">
-              {flightsData.length} Tickets Available
+              {!isLoading && `${data?.length} Flights Available`}
             </h6>
           </div>
-          {flightsData.map((flight) => (
-            <div
-              className="p-5 flex flex-col gap-5 md:flex-row md:items-center bg-white rounded-md justify-between mb-5"
-              key={flight.id}
-            >
-              {/* Airline Info */}
-              <div className="md:flex md:flex-col hidden  justify-center items-center">
-                <img className="w-20" src={flight.img} alt={flight.airline} />
-                <p>{flight.airline}</p>
-              </div>
-              {/* Timeline */}
-              <div className="flex justify-between">
-                <div className="md:hidden block">
+          {!isLoading &&
+            data?.map((flight) => (
+              <div
+                className="p-5 flex flex-col gap-5 md:flex-row md:items-center bg-white rounded-md justify-between mb-5"
+                key={flight._id}
+              >
+                {/* Airline Info */}
+                <div className="md:flex md:flex-col hidden  justify-center items-center">
                   <img className="w-20" src={flight.img} alt={flight.airline} />
                   <p>{flight.airline}</p>
                 </div>
-                <ul className="timeline">
-                  <li>
-                    <div className="timeline-start">{flight.departureTime}</div>
-                    <div className="timeline-middle">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="h-5 w-5"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </div>
-                    <div className="timeline-end ">{flight.departureCity}</div>
-                    <hr />
-                  </li>
-                  <li>
-                    <hr />
-                    <div className="timeline-start">{flight.duration}</div>
-                    <div className="timeline-middle">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="h-5 w-5"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </div>
-                    <div className="timeline-end">{flight.stops}</div>
-                    <hr />
-                  </li>
-                  <li>
-                    <hr />
-                    <div className="timeline-start">{flight.arrivalTime}</div>
-                    <div className="timeline-middle">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="h-5 w-5"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </div>
-                    <div className="timeline-end">{flight.arrivalCity}</div>
-                  </li>
-                </ul>
-              </div>
-              {/* Price */}
-              <div className="font-bold text-2xl hidden md:block">
-                {flight.price}
-              </div>
-              {/* Buttons */}
-              <div className="md:flex md:flex-col gap-1 hidden ">
-                <button
-                  className="btn md:btn-md btn-sm"
-                  onClick={() =>
-                    document.getElementById("my_modal_3").showModal()
-                  }
-                >
-                  View Details
-                </button>
-                <button className="btn md:btn-md btn-sm">Book Now</button>
-              </div>
-              {/* Mobile View */}
-              <div className="flex justify-between items-center w-full md:hidden">
-                <p className="font-bold text-2xl">{flight.price}</p>
-                <div className="flex flex-col gap-1">
+                {/* Timeline */}
+                <div className="flex justify-between">
+                  <div className="md:hidden block">
+                    <img
+                      className="w-20"
+                      src={flight.img}
+                      alt={flight.airline}
+                    />
+                    <p>{flight.airline}</p>
+                  </div>
+                  <ul className="timeline">
+                    <li>
+                      <div className="timeline-start">{flight.time}</div>
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="h-5 w-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="timeline-end ">{flight.origin}</div>
+                      <hr />
+                    </li>
+                    <li>
+                      <hr />
+                      <div className="timeline-start">05h 15m</div>
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="h-5 w-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="timeline-end">Non stop</div>
+                      <hr />
+                    </li>
+                    <li>
+                      <hr />
+                      <div className="timeline-start">unknwon</div>
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="h-5 w-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="timeline-end">{flight.destination}</div>
+                    </li>
+                  </ul>
+                </div>
+                {/* Price */}
+                <div className="font-bold text-2xl hidden md:block">
+                  {flight.price}
+                </div>
+                {/* Buttons */}
+                <div className="md:flex md:flex-col gap-1 hidden ">
                   <button
                     className="btn md:btn-md btn-sm"
                     onClick={() =>
@@ -271,9 +272,23 @@ export const Flights = () => {
                   </button>
                   <button className="btn md:btn-md btn-sm">Book Now</button>
                 </div>
+                {/* Mobile View */}
+                <div className="flex justify-between items-center w-full md:hidden">
+                  <p className="font-bold text-2xl">{flight.price}</p>
+                  <div className="flex flex-col gap-1">
+                    <button
+                      className="btn md:btn-md btn-sm"
+                      onClick={() =>
+                        document.getElementById("my_modal_3").showModal()
+                      }
+                    >
+                      View Details
+                    </button>
+                    <button className="btn md:btn-md btn-sm">Book Now</button>
+                  </div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
