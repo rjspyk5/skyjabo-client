3fb4a36e4108f0043202525d15ae606cc081342c
rjspyk5 <rjspyk5@gmail.com>
2024-12-02 12:53:29 +0600

    desgin error page and axiosSeqrue setup

--- src/hooks/useAxiosSequre.jsx
+++ src/hooks/useAxiosSequre.jsx
@@ -1,9 +1,26 @@
 import axios from "axios";
+import { useAuth } from "./useAuth";
+import { useNavigate } from "react-router";
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
 
 export const useAxiosSequre = () => {
-  const axiosPubilc = axios.create({
-    baseURL: "http://localhost:5000",
-    withCredentials: true,
-  });
-  return axiosPubilc;
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  axiosSecure.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (error) => {
+      if (error.response.status === 401 || error.response.status === 403) {
+        logout().then(() => navigate("/login"));
+      }
+    }
+  );
+
+  return axiosSecure;
 };
